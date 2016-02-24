sidpagariya/atom

Send check-for-update message over ipc

src/browser/atom-application.coffee
@@ -283,6 +283,9 @@ class AtomApplication
     ipc.on 'add-recent-document', (event, filename) ->
       app.addRecentDocument(filename)
 
+    ipc.on 'check-for-update', =>
+      @autoUpdateManager.check()
+
   setupDockMenu: ->
     if process.platform is 'darwin'
       dockMenu = Menu.buildFromTemplate [

srikanth1982/DryRun1

src/browser/atom-application.coffee
@@ -304,6 +304,9 @@ class AtomApplication
     ipcMain.on 'execute-javascript-in-dev-tools', (event, code) ->
       event.sender.devToolsWebContents?.executeJavaScript(code)
 
+    ipcMain.on 'check-for-update', =>
+      @autoUpdateManager.check()
+
   setupDockMenu: ->
     if process.platform is 'darwin'
       dockMenu = Menu.buildFromTemplate [